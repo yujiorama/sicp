d29c401ff8b76318cdaaab67d62e4f8b924cae9b
yujiorama <yujiorama+github@gmail.com>
2009-09-28 23:17:34 +0900

    3.5.3 を写経

--- chapter3.scm
+++ chapter3.scm
@@ -72,3 +72,44 @@
   (cons-stream
    (quotient (* num radix) den)
    (expand (remainder (* num radix) den) den radix)))
+
+(define (sqrt-improve guess x)
+  (define (average a b)
+    (/ (+ a b) 2))
+  (average guess (/ x guess)))
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                              (sqrt-improve guess x))
+                            guesses)))
+  guesses)
+
+;; (load "./ex355.scm")
+(define (partial-sums s)
+  (cons-stream
+   (stream-car s)
+   (add-stream (stream-cdr s) (partial-sums s))))
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform
+                             (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
