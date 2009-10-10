ab39568298aa95e07682b773fd5db4456cb0ced3
yujiorama <yujiorama+github@gmail.com>
2009-10-10 22:40:21 +0900

    写経が消えてたので追加

--- chapter3.scm
+++ chapter3.scm
@@ -113,3 +113,18 @@
 (define (accelerated-sequence transform s)
   (stream-map stream-car
               (make-tableau transform s)))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
