b0ec2229c2ff2adfc06e41c0d18bfa67f8957d8a
yujiorama <yujiorama+github@gmail.com>
2010-09-05 15:48:56 +0900

    3.5.5 の説明の本文コードを写経したけど、なんか無限ループが起きてるぽい

--- chapter3.scm
+++ chapter3.scm
@@ -158,3 +158,43 @@
 ;;   y)
 
 
+
+;; 3.5.5
+
+;; (define rand
+;;   (let ((x random-init))
+;;     (lambda ()
+;;       (set! x (rand-update x))
+;;       x)))
+(use srfi-27)
+(define random-init
+  (random-integer (sys-time)))
+(define rand-update
+  (random-integer (sys-time)))
+
+(define (random-numbers)
+  (cons-steram random-init
+               (stream-map rand-update (random-numbers))))
+
+(define (map-successive-pairs proc s)
+  (cons-stream
+   (proc (stream-car s) (stream-car (stream-cdr s)))
+   (map-successive-pairs proc (stream-cdr (stream-cdr s)))))
+
+(define cesaro-stream
+  (map-successive-pairs (lambda (r1 r2) (= (gcd r1 r2) 1))
+                        random-numbers))
+
+(define (monte-carlo experiment-stream passed failed)
+  (define (next passed failed)
+    (cons-stream
+     (/ passed (+ passed failed))
+     (monte-carlo
+      (stream-cdr experiment-stream) passed failed)))
+  (if (stream-car experiment-stream)
+      (next (+ passed 1) failed)
+      (next passed (+ 1 failed))))
+
+(define pi
+  (stream-map (lambda (p) (sqrt (/ 6 p)))
+              (monte-carlo cesaro-stream 0 0)))
