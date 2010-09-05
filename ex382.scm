c44ad7d3eab737c7d55adf4dd71ecc02f31cddcb
yujiorama <yujiorama+github@gmail.com>
2010-09-05 23:12:57 +0900

    それっぽい数字が出るけどうさんくさい

--- ex382.scm
+++ ex382.scm
@@ -1,16 +1,44 @@
+(load "./chapter3.scm")
 
+(define (random-in-range a b)
+  (lambda (x)
+    (let ((xx (remainder x b)))
+      (if (< xx a) (+ a xx) xx))))
 
 (define (estimate-integral p x1 x2 y1 y2)
-  (define (random-in-range a b)
-    (lambda (x)
-      (let ((xx (remainder x b)))
-        (if (< a xx) (+ a xx) xx))))
+  (define x-stream
+    (stream-map (random-in-range x1 x2) random-numbers))
+  (define y-stream
+    (stream-map (random-in-range y1 y2) random-numbers))
+  (define area
+    (* (- x2 x1) (- y2 y1)))
   (stream-map
-   (lambda (x) (* p (* (- x2 x1) (- y2 y1))))
+   (lambda (x) (* area x))
    (monte-carlo
-    (stream-map (random-in-range x1 x2) cesaro-stream)
-    (stream-map (random-in-range y1 y2) cesaro-stream)
-    0 0)
-   )
+    (stream-map
+     (lambda (x) (p (car x) (cadr x)))
+     (pairs x-stream y-stream)) 0 0))
   )
 
+;;
+;; 中心が (5,5) で半径が 5 を想定
+;; 
+(define (estimate-pi index)
+  (define radius 5)
+  (define x-center 5)
+  (define y-center 5)
+  (define (P x y)
+    (<= (+ (square (- x x-center)) (square (- y y-center))) (square radius)))
+  (define estimate
+    (estimate-integral
+     P
+     (- x-center radius) (+ x-center radius)
+     (- y-center radius) (+ y-center radius)
+     )
+    )
+  (for-each
+   (lambda (x)
+     (display (/ (stream-ref estimate x) (square radius)))
+     (newline)
+     )
+   index))
