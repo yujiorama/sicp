ceac6f53b33f6fdde8d69104d2fe123415b4e9a2
yujiorama <yujiorama+github@gmail.com>
2010-10-10 21:03:18 +0900

    exps が list だとすれば、first-operand が car になり、rest-operands が cdr になればそれでよいと思った。 右から左の場合は exps を reverse したやつを let した環境で eval してればいいと思った。

--- ex41.scm
+++ ex41.scm
@@ -12,8 +12,23 @@
 ;;
 ;; 左から右へ評価
 ;;
-
+(define (list-of-values exps env)
+  (define first-operand car)
+  (define rest-operands cdr)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
 
 ;;
 ;; 右から左へ評価
 ;;
+(define (list-of-values exps env)
+  (define first-operand car)
+  (define rest-operands cdr)
+  (let ((rexps (reverse exps)))
+    (if (no-operands? exps)
+        '()
+        (cons (eval (first-operand rexps) env)
+              (list-of-values (rest-operands rexps) env)))))
+
