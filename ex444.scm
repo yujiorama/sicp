520af0f94eb57738d5113d37edcf7d49ea8a0fe3
yujiorama <yujiorama+github@gmail.com>
2010-11-17 23:51:43 +0900

    書き直して eval-or も定義した。

--- ex444.scm
+++ ex444.scm
@@ -4,11 +4,26 @@
   (tagged-list? exp 'and))
 
 (define (eval-and exp env)
-  (define (iter exp)
-    (let ((result (eval (car exp) env)))
-      (if result
-          (if (null? (cdr exp))
-              result
-              (iter (cdr exp)))
-          #f)))
-  (iter (cdr exp)))
+  (define (iter result rest)
+    (if (false? result)
+        #f
+        (if (null? rest)
+            #f
+            (iter (eval (car rest) env) (cdr rest)))))
+  (if (null? (cdr exp))
+      #t
+      (iter (eval (cadr exp) env) (cddr exp))))
+
+(define (or? exp)
+  (tagged-list? exp 'or))
+
+(define (eval-or exp env)
+  (define (iter result rest)
+    (if result
+        result
+        (if (null? rest)
+            #f
+            (iter (eval (car rest) env) (cdr rest)))))
+  (if (null? (cdr exp))
+      #f
+      (iter (eval (cadr exp) env) (cddr exp))))
