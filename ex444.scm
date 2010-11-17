3449cc3c05b6e04e5338dd1624ba9026250e6a8a
yujiorama <yujiorama+github@gmail.com>
2010-11-17 23:53:14 +0900

    eval に組み込んだ。

--- ex444.scm
+++ ex444.scm
@@ -27,3 +27,26 @@
   (if (null? (cdr exp))
       #f
       (iter (eval (cadr exp) env) (cddr exp))))
+
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((and? exp) (eval-and exp env))
+        ((or? exp) (eval-or exp env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unkown expression type -- EVAL" exp))))
