b05bc6994a60e2d836c5d8e4b8f28141ac81d48d
yujiorama <yujiorama+github@gmail.com>
2010-11-17 22:48:29 +0900

    動かせてないけどこれでいいと思う。

--- ex441.scm
+++ ex441.scm
@@ -1,15 +1,18 @@
+(load "./chapter4.scm")
+
 ;; 基盤の lisp が cons を右から評価するか左から評価するかを問わず演算子を左から評価する list-of-values
-(define (list-of-values exps env)
+(define (list-of-values-l exps env)
   (if (no-operands? exps)
       '()
       (let ((lhs (eval (first-operand exps) env)))
         (cons lhs 
-              (list-of-values (rest-operands exps) env)))))
+              (list-of-values-l (rest-operands exps) env)))))
 
 ;;右から左へ評価する list-of-values
-(define (list-of-values exps env)
+(define (list-of-values-r exps env)
   (if (no-operands? exps)
       '()
-      (let ((rhs (list-of-values (rest-operand exps) env)))
+      (let ((rhs (list-of-values-r (rest-operands exps) env)))
         (cons (eval (first-operand exps) env)
               rhs))))
+
