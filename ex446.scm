7baf1fbffe384541d4949f598e6e4476565f5922
yujiorama <yujiorama+github@gmail.com>
2010-11-20 15:12:48 +0900

    lambda 式を組み立てる let->combination と、適用する引数を取り出す let-values を追加してみた。

--- ex446.scm
+++ ex446.scm
@@ -1,2 +1,33 @@
 ;; let は lambda で等価な式が書けるから、let は導出式
 ;; let を lambda にする let->combination を実装して、eval に組み込む
+
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let->combination exp)
+  (make-lambda (map (lambda (x) (car x)) (cadr exp))
+               (cddr exp)))
+
+(define (let-values exp)
+  (map (lambda (x) (cadr x)) (cadr exp)))
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((let? exp) (eval (cons (let->combination exp) (let-values exp)) env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+         (error "Unkown expression type -- EVAL" exp))))
