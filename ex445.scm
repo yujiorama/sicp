3d50d062aaf27d3f826d2b721940ee55855ab9ba
yujiorama <yujiorama+github@gmail.com>
2010-11-20 15:00:25 +0900

    変数のタイポを修正。不要な let を消す。

--- ex445.scm
+++ ex445.scm
@@ -2,7 +2,7 @@
 ;; expand-clause を修正します。
 
 (define (expand-clause clauses)
-  (if (null? clause)
+  (if (null? clauses)
       #f
       (let ((first (car clauses))
             (rest (cdr clauses)))
@@ -10,12 +10,11 @@
             (if (null? rest)
                 (sequence->exp (cond-actions first))
                 (error "ELSE clause isn't last -- COND->IF" clauses))
-            (let* ((predicate (cond-predicate first))
-                   (actions (cond-actions first))
-                   (first-action (car actions)))
-              (if (eq? '=> first-action)
+            (let ((predicate (cond-predicate first))
+                   (actions (cond-actions first)))
+              (if (eq? '=> (car actions))
                   (if (eval predicate env)
-                      (eval (cons actions predicate) env))
+                      (eval (cons (cdr actions) predicate) env))
                   (make-if predicate
                      (sequence->exp actions)
                      (expand-clause rest))))))))
