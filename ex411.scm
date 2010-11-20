cddc9417171b273096a0dea2eb7ca417ba6d9af4
yujiorama <yujiorama+github@gmail.com>
2010-11-21 00:02:26 +0900

    define-variable! も変更。

--- ex411.scm
+++ ex411.scm
@@ -42,10 +42,5 @@
 
 (define (define-variable! var val env)
   (let ((frame (first-frame env)))
-    (define (scan vars vals)
-      (cond ((null? vars)
-             (add-binding-to-frame! var val frame))
-            ((eq? var (car vars))
-             (set-car! vals val))
-            (else (scan (cdr vars) (cdr vals)))))
-    (scan (frame-variables frame) (frame-values frame))))
+    (cond ((assoc var frame) (set-cdr! (assoc var frame) val))
+          (else (add-binding-to-frame! var val frame)))))
