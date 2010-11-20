c936b38461e9a6b83d9337c136c2cc54cb9b6468
yujiorama <yujiorama+github@gmail.com>
2010-11-20 16:02:14 +0900

    それぞれの導出式としての定義例を追加

--- ex449.scm
+++ ex449.scm
@@ -1,10 +1,10 @@
 ; do for while until のような反復構造の設計と使用例
 
-(for <vars> <loop-condition> <incremental> <proc>)
+(for <vars> <loop-condition> <inc> <proc>)
 
 vars は反復の最初に 1 度だけ初期化される変数と式の対からなるリスト。
-loop-condition は引数を取らない真または偽を返す手続き。
-incremental は反復ごとに評価される引数を取らない手続き。
+loop-condition は反復ごとに評価される引数を取らない手続き。真または偽を返す。
+inc は反復ごとに評価される引数を取らない手続き。
 proc は反復ごとに評価される引数を取らない手続き。
 
 <loop-condition> が偽になったら反復を止める。式の値はない。
@@ -16,4 +16,17 @@ proc は反復ごとに評価される引数を取らない手続き。
 <loop-condition> が偽になったら反復を止める。式の値はない。
 
 (until <loop-condition> <proc>)
-(while (not (<loop-condition>) <proc>)) と同じ。 
+(while (not (<loop-condition>) <proc>)) と同じ。
+
+(define (for vars loop-condition inc proc)
+  (let vars
+      (define (iter)
+        (cond ((loop-condition) (eval proc env) (inc) (iter))
+              (else #f)))
+    (iter)))
+
+(define (while loop-condition proc)
+  (let vars
+      (define (iter)
+        (cond ((loop-condition) (eval proc env) (iter)))
+              (else #f))))
