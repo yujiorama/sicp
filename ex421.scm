05e7f9357f46b1b044abe41256d48c59fa9318d4
yujiorama <yujiorama+github@gmail.com>
2011-01-02 19:07:10 +0900

    <??> を埋めた。

--- ex421.scm
+++ ex421.scm
@@ -51,6 +51,6 @@
   ((lambda (even? odd?)
      (even? even? odd? x))
    (lambda (ev? od? n)
-     (if (= n 0) #t (od? <??> <??> <??>)))
+     (if (= n 0) #t (od? ev? od? (- n 1))))
    (lambda (ev? od? n)
-     (if (= n 0) #f (ev? <??> <??> <??>)))))
+     (if (= n 0) #f (ev? ev? od? (- n 1))))))
