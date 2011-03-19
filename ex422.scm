2e0135dad3cbdc3dd0a376e71fa1713ab66bdb95
yujiorama <yujiorama+github@gmail.com>
2011-03-19 16:48:47 +0900

    4.6 の丸々コピー。いいのかな #sicp-lite

--- ex422.scm
+++ ex422.scm
@@ -1,2 +1,38 @@
 (load "./chapter4a.scm")
 
+;; 本体の評価器を拡張し、特殊形式 let が使えるようにせよ
+(define (let? exp)
+  (tagged-list? exp 'let))
+
+(define (let->combination exp)
+  (make-lambda (map car (cadr exp))
+               (cddr exp)))
+
+(define (let-values exp)
+  (map cadr (cadr exp)))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+         (analyze-self-evaluating exp))
+        ((quoted? exp)
+         (analyzed-quoted exp))
+        ((variable? exp)
+         (analyze-variable exp))
+        ((assignment? exp)
+         (analyze-assignment exp))
+        ((definition? exp)
+         (analyze-definition exp))
+        ((if? exp)
+         (analyze-if exp))
+        ((let? exp)
+         (analyze (let->combination exp)))
+        ((lambda? exp)
+         (analyze-lambda exp))
+        ((begin? exp)
+         (analyze-sequence (begin-actions exp)))
+        ((cond? exp)
+         (analyze (cond->if exp)))
+        ((application? exp)
+         (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
